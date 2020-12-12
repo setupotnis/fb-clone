60d51eedca120c6184b45219e894d98f849f22ed
Setu Potnis <setupotnis@gmail.com>
2020-12-12 18:49:02 -0500

    added widgets component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
+  const user = null;
   return (
     // BEM naming convention
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {!user ? (
+        <h1>Login</h1>
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+            {/* Widgets */}
+          </div>
+        </>
+      )}
     </div>
   );
 }
